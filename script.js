40767f8d13a70908dd06b279f8633090daf10cbf
fabricio lima <fabriciochelseablack@gmail.com>
2022-04-13 00:19:24 -0300

    Login com animação

---
+++ script.js
@@ -0,0 +1,16 @@
+
+/*
+Aqui estou adicinando a id ativar e ja estou colocando um evento nele
+depois do pegndo o id modal e adicionando uma class animar nela e tbm tirando a class
+atraves do toggler
+ */
+
+ativar.addEventListener('click' , ()=>{
+    modal.classList.toggle('animar')
+})
+
+// aqui estou pegando o id logar e ao receber um click estou adicionando uma clas no loand 
+
+logar.addEventListener('click', ()=>{
+    load.classList.toggle('load')
+})
